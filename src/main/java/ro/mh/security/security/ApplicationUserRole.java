39f0bf2588e23cac7b7846423e17cdc1ae59df31
Marius <mariusherlea@gmail.com>
2021-11-13 09:43:39 +0200

    Permission Based Authentication

--- src/main/java/ro/mh/security/security/ApplicationUserRole.java
+++ src/main/java/ro/mh/security/security/ApplicationUserRole.java
@@ -1,8 +1,11 @@
 package ro.mh.security.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static ro.mh.security.security.ApplicationUserPermission.*;
 
@@ -19,5 +22,14 @@ public enum ApplicationUserRole {
 
     public Set<ApplicationUserPermission> getPermissions() {
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permission = getPermissions().stream()
+                .map(permissions -> new SimpleGrantedAuthority(permissions.getPermission()))
+                .collect(Collectors.toSet());
+
+        permission.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return permission;
     }
 }
